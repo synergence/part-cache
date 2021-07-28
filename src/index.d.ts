0a66a5d9f3b1b5d88ff1da83de66342dc520cb83
xethlyx <46338199+xethlyx@users.noreply.github.com>
2021-07-28 02:03:27 -0400

    Add generics

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,7 +4,7 @@ interface PartCacheConstructor {
 	/**
 	 * Creates a new part cache using the specified template part, creating precreatedParts parts to grab from the cache. This will error if template is nil or precreatedParts < 0. By default precreatedParts is 5.
 	 */
-	new(template: BasePart, precreatedParts: number): PartCache;
+	new<T extends BasePart = BasePart>(template: T, precreatedParts: number): PartCache<T>;
 }
 
 declare const PartCacheModule: PartCacheConstructor;
